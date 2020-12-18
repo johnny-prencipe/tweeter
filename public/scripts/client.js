890985c932b1cbd5b70ca66abf1507bc57f53e14
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-18 07:59:05 +0000

    add jquery to submit new tweet, comment out tester tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    $("#tweet-container").prepend(createTweetElement(tweet));
+  }
+}
+
+function createTweetElement(tweet) {
+  const $newTweet = `
+
+  <header class="tweet">
+    <div class="display-name">
+      <img src="tweet.user.avatars" class="user-image"></img>
+      ${tweet.user.name}
+    </div>
+    <div class="userID">${tweet.user.handle}</div>
+  </header>
+  ${tweet.content.text}
+  <footer class="tweet">${tweet.created_at}</footer>
+  `;  
+  return $newTweet;
+}  
+
+
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $("#tweet-text").on("input", function() {
-      let length = $(this).val().length;
-      let $counter = $(".counter");
+  $('#tweet-text').on('input', function() {
+      const length = $(this).val().length;
+      const $counter = $('.counter');
       $counter.val(140 - length);
 
       $counter.val() < 0 ?
-      $counter.css({ color: "red" }) :
-      $counter.css({ color: "#545149" });
+      $counter.css({ color: 'red' }) :
+      $counter.css({ color: '#333' });
   });
 });
