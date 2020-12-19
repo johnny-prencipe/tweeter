9867e288a427d4034164f1ada522480628448298
Johnny Prencipe <johnny.prencipe@gmail.com>
2020-12-19 01:09:30 +0000

    implement AJAX logic to post new tweet to server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,45 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+  console.log('Document ready');
 
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    $("#tweet-container").prepend(createTweetElement(tweet));
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
   }
-}
 
-function createTweetElement(tweet) {
-  const $newTweet = `
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $("#tweet-container").prepend(createTweetElement(tweet));
+    }
+  }
 
-  <header class="tweet">
-    <div class="display-name">
-      <img src="tweet.user.avatars" class="user-image"></img>
-      ${tweet.user.name}
-    </div>
-    <div class="userID">${tweet.user.handle}</div>
-  </header>
-  ${tweet.content.text}
-  <footer class="tweet">${tweet.created_at}</footer>
-  `;  
-  return $newTweet;
-}  
+  function createTweetElement(tweet) {
+    const $newTweet = `
+
+    <header class="tweet">
+      <div class="display-name">
+        <img src="${tweet.user.avatars}" class="user-image"></img>
+        ${tweet.user.name}
+      </div>
+      <div class="userID">${tweet.user.handle}</div>
+    </header>
+    ${tweet.content.text}
+    <footer class="tweet">${tweet.created_at}</footer>
+    `;  
+    return $newTweet;
+  }
 
 
+  $('.tweet-form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: '/tweets',
+      type: 'POST',
+      data: $(this).serialize()
+    }).then(console.log('Post made'));
+  });
 
-const $tweet = createTweetElement(tweetData);
+  function newTweet(event) {
+    // event.preventDefault();
+    // $('new-tweet form').on('submit', function(event) {
+    //   console.log('asd');
+    // });
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+    const tweetText = $('.new-tweet form #tweet-text').val();
+  }
+
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
